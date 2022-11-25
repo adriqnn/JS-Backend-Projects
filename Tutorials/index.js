e41262b146cb9bb47427e3fb208d5a9ba204e8c4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2022-11-25 05:37:16 +0200

    tutorials exam prep

---
+++ Tutorials/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const databaseConfig = require('./config/database');
+const expressConfig = require('./config/express');
+const routesConfig = require('./config/routes');
+
+async function start(){
+    const app = express();
+    
+    expressConfig(app);
+    routesConfig(app);
+    await databaseConfig(app);
+    
+    app.listen(3000, () => console.log('Server listening on port 3000'));
+}
+
+start();
