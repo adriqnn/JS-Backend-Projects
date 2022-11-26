3acaec3b34afe8d7291a67daf50e8915cc40c9fc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2022-11-26 23:02:53 +0200

    booking exam prep

---
+++ Booking/services/hotelService.js
@@ -0,0 +1,51 @@
+const Hotel = require("../models/Hotel")
+
+async function getAll(){
+    return Hotel.find({}).lean();
+};
+
+async function getById(id){
+    return Hotel.findById(id).lean();
+};
+
+async function create(hotel){
+    // const name = hotel.name;
+    // const hotelExists = await Hotel.findOne({name}).collation({locale: 'en', strength:2});
+    // if(hotelExists){
+    //     throw new Error('Hotel already in the DB');
+    // }
+    return await Hotel.create(hotel);
+};
+
+async function update(id, hotel){
+    const existing = await Hotel.findById(id);
+    existing.name = hotel.name;
+    existing.city = hotel.city;
+    existing.imageUrl = hotel.imageUrl;
+    existing.rooms = hotel.rooms;
+    await existing.save();
+};
+
+async function deleteById(id){
+    await Hotel.findByIdAndRemove(id);
+};
+
+async function bookRoom(hotelId, userId){
+    const hotel = await Hotel.findById(hotelId);
+    hotel.bookings.push(userId);
+    await hotel.save();
+};
+
+async function getByUserBooking(userId){
+    return await Hotel.find({booking: userId}).lean();
+};
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    deleteById,
+    bookRoom,
+    getByUserBooking
+};
